a0651b090fe8e5ef94ff65688febcf4016513e5c
JonathanArns <jonathan.arns@googlemail.com>
2019-10-22 15:09:24 +0200

    key hashing job implemented

--- src/main/java/main/StreamingJob.java
+++ src/main/java/main/StreamingJob.java
@@ -18,51 +18,42 @@
 
 package main;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
-
 import org.apache.flink.streaming.connectors.kafka.internals.KeyedSerializationSchemaWrapper;
 
 import java.util.Properties;
 
 /**
- * Skeleton for a Flink Streaming Job.
+ * Flink Streaming Job to generate primary keys for json messages in kafka
  *
- * <p>For a tutorial how to write a Flink streaming application, check the
- * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
  */
 public class StreamingJob {
 
 	public static void main(String[] args) throws Exception {
-		String jobName = "example_job";
-		String inputTopic = "test_topic";
-		String outputTopic = "test_topic_persist";
-		String consumerGroup = "groupID";
-		String kafkaAddress = "localhost:9092"; // for running in eclipse use "localhost:9092", for flink cluster "kafka:29092"
-		String jsonOutputSchema = "{\"schema\":{\"type\":\"struct\",\"fields\":[{\"type\":\"string\",\"optional\":true," +
-				"\"field\":\"first_name\"},{\"type\":\"string\",\"optional\":true,\"field\":\"last_name\"}]," +
-				"\"optional\":false,\"name\":\"com.github.jcustenborder.kafka.connect.model.Value\"}}";
+		ParameterTool parameterTool = ParameterTool.fromArgs(args);
+
+		String jobName = parameterTool.get("job-name", "KeyHashingJob");
+		String inputTopic = parameterTool.get("input-topic", "test_topic");
+		String outputTopic = parameterTool.get("output-topic", "test_topic_persist");
+		String consumerGroup = parameterTool.get("group-id", "KeyHashingGroup");
+		String kafkaAddress = parameterTool.get("kafka-address", "localhost:9092"); // for running in eclipse use "localhost:9092", for flink cluster "kafka:29092"
 
 		ObjectMapper objectMapper = new ObjectMapper();
 
 		//get the execution environment
 		StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		//create a new kafka consumer -> this is where your data comes from
+		//create a new kafka consumer
 		Properties consumerProps = new Properties();
 		consumerProps.setProperty("bootstrap.servers", kafkaAddress);
 		consumerProps.setProperty("group.id",consumerGroup);
@@ -72,23 +63,21 @@ public class StreamingJob {
 		//add the consumer to the environment as a data-source, to get a DataStream
 		DataStream<String> dataStream = environment.addSource(flinkKafkaConsumer);
 
-		//parse the json messages
-		DataStream<ObjectNode> jsonStream = dataStream.map((MapFunction<String, ObjectNode>) value -> (ObjectNode)objectMapper.readTree(value));
-
-		DataStream<ObjectNode> jsonPayloadStream = jsonStream
-				//remove schemas
-				.map((MapFunction<ObjectNode, ObjectNode>) value -> (ObjectNode)value.get("payload"))
-				//example calculation
-				.map((MapFunction<ObjectNode, ObjectNode>) value -> value.put("first_name", value.get("first_name").textValue().toUpperCase()));
-
-		//TODO: your calculations
-
-		// introduce schema and format to the output string for kafka
-		DataStream<String> outputStream = jsonPayloadStream
-				.map((MapFunction<ObjectNode,ObjectNode>) value -> ((ObjectNode)objectMapper.readTree(jsonOutputSchema)).set("payload", value))
+		// calculation
+		DataStream<String> outputStream = dataStream
+				// parse the json string
+				.map((MapFunction<String, ObjectNode>) value -> (ObjectNode)objectMapper.readTree(value))
+				// do the calculation and add the hash to the payload as well as its definition to the schema
+				.map((MapFunction<ObjectNode, ObjectNode>) value -> {
+					((ObjectNode) value.get("payload")).put("postgres_pk", DigestUtils.sha256Hex(objectMapper.writeValueAsBytes(value)));
+					((ArrayNode) value.get("schema").get("fields")).add(objectMapper.createObjectNode()
+							.put("type","string").put("optional",false).put("field","postgres_pk"));
+					return value;
+				})
+				// convert to string again
 				.map((MapFunction<ObjectNode, String>) value -> objectMapper.writeValueAsString(value));
 
-		//create a new kafka producer -> this is where your results will go
+		//create a new kafka producer
 		Properties producerProps = new Properties();
 		producerProps.setProperty("bootstrap.servers", kafkaAddress);
 		FlinkKafkaProducer<String> flinkKafkaProducer = new FlinkKafkaProducer<>(outputTopic,
