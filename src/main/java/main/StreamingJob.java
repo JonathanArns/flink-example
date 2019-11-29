1983256ef9514e7725601ad14e473eb02be6ccd6
JonathanArns <jonathan.arns@googlemail.com>
2019-11-29 09:23:44 +0100

    change default topics

--- src/main/java/main/StreamingJob.java
+++ src/main/java/main/StreamingJob.java
@@ -43,8 +43,8 @@ public class StreamingJob {
 		ParameterTool parameterTool = ParameterTool.fromArgs(args);
 
 		String jobName = parameterTool.get("job-name", "KeyHashingJob");
-		String inputTopic = parameterTool.get("input-topic", "test_topic");
-		String outputTopic = parameterTool.get("output-topic", "test_topic_persist");
+		String inputTopic = parameterTool.get("input-topic", "transaction_data");
+		String outputTopic = parameterTool.get("output-topic", "transaction_data_persist");
 		String consumerGroup = parameterTool.get("group-id", "KeyHashingGroup");
 		String kafkaAddress = parameterTool.get("kafka-address", "kafka:29092"); // for running in eclipse use "localhost:9092", for flink cluster "kafka:29092"
 
